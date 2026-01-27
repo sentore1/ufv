3d3f866db893aeffbc73b813b1fc4c0ed44fbc7c
sentore1 <abdousentore@gmail.com>
2026-01-28 01:59:35 +0200

    nknknjknjnkjnjk

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-const nextConfig: NextConfig = {
-  experimental: {
-    skipTrailingSlashRedirect: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
